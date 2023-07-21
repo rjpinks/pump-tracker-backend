6b0bd39cdbf0c68170b6caa2322068db02d41a5c
Ryan <pinkstonr54@gmail.com>
2023-07-20 23:18:48 -0500

    added all strength routes

--- routes/api/strengthRoutes.js
+++ routes/api/strengthRoutes.js
@@ -10,4 +10,73 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const strengthData = await Strength.findByPk(req.params.id)
+
+    if (!strengthData) {
+      res.status(404).json({ message: 'Wrong ID buddy ☹️'});
+    }
+
+    res.status(200).json(strengthData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const strengthData = await Strength.create({
+      date: req.body.date, /* the front-end needs to pull the date from the browser */
+      activity: req.body.activity,
+      reps: req.body.reps,
+      weight: req.body.weight,
+      satisfaction: req.body.satisfaction,
+      remarks: req.body.remarks,
+      profile_id: req.body.profile_id
+    });
+    res.status(200).json(strengthData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  Strength.update({
+    activity: req.body.activity,
+    reps: req.body.reps,
+    weight: req.body.weight,
+    satisfaction: req.body.satisfaction,
+    remarks: req.body.remarks
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }
+  )
+    .then((updatedHiit) => {
+      res.json(updatedHiit);
+  })
+    .catch((err) => res.json(err));
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const strengthData = await Strength.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!strengthData) {
+      res.status(404).json({ message: "Can't delete what you can't touch 🥊" });
+    }
+
+    res.status(200).json(strengthData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
 module.exports = router;
